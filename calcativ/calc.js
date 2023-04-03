6389192b4bfbf15fc6e0eff10d5dd8b6162864b0
Arthur Cavalcante <arthurcavalcante0107@gmail.com>
2023-04-03 16:22:15 -0300

    exercicios para revisão

---
+++ calcativ/calc.js
@@ -0,0 +1,10 @@
+
+
+function inserir(num)
+{
+    let num1 = getElementsById('num1');
+    let num2 = getElementsById('num2')
+    let resultado = document.getElementsById('result').innerHTML = `${'num1'}+${'num2'}`;
+}
+
+//eu apaguei umas coisas a mais que eu botei pq tava errado tambem
---
+++ formativ/frm.js
@@ -0,0 +1,14 @@
+
+
+    function nomeCompleto(){
+    let nome = document.getElementByClass("nome")
+    let sobrenome = document.getElementsByClass("sobrenome")
+    let nomeCompleto = `${nome} ${sobrenome}`
+    
+}
+
+    console.log(nomeCompleto)
+
+let clicar = document.getElementById(button)
+
+
