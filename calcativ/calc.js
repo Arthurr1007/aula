67ec1d7c37951d7e233b495296385fab6b70edf5
Arthur Cavalcante <arthurcavalcante0107@gmail.com>
2023-04-04 10:09:53 -0300

    alterações feitas

--- calcativ/calc.js
+++ calcativ/calc.js
@@ -1,10 +1,11 @@
 
 
-function inserir(num)
+function inserir()
 {
-    let num1 = getElementsById('num1');
-    let num2 = getElementsById('num2')
-    let resultado = document.getElementsById('result').innerHTML = `${'num1'}+${'num2'}`;
+    let num1 = document.getElementById ('num1');
+    let num2 = document.getElementById ('num2');
+    let result = document.getElementById('result').value = `${'num1'}+${'num2'}`;
 }
+
 
 //eu apaguei umas coisas a mais que eu botei pq tava errado tambem
