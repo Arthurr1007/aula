88f1c093c57a26067d2870d0a59dcb14f2b2ae8f
Arthur Cavalcante <arthurcavalcante0107@gmail.com>
2023-04-05 09:41:49 -0300

    calculadora + funcionando

--- formativ/frm.js
+++ formativ/frm.js
@@ -1,14 +1,13 @@
 
-
-    function nomeCompleto(){
-    let nome = document.getElementByClass("nome")
-    let sobrenome = document.getElementsByClass("sobrenome")
-    let nomeCompleto = `${nome} ${sobrenome}`
+    function escreverNomeCompleto(){
+    var nome = document.getElementById("nome")
+    var sobrenome = document.getElementById("sobrenome")
+    var nomeCompleto = nome + sobrenome//`${nome} ${sobrenome}`
     
 }
 
-    console.log(nomeCompleto)
+console.log(nomeCompleto)
 
-let clicar = document.getElementById(button)
+
 
 
